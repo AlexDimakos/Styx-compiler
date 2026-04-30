e9486314333b3c491b2ab6adffa707a054cb0c04
alex <alexander.dimakos20@gmail.com>
2026-04-30 21:43:58 +0200

    Examples update

---
+++ examples/benchmarks/original/ycsb.py
@@ -0,0 +1,41 @@
+from styx_compiler.core import entity, send_async
+
+class NotEnoughCredit(Exception):
+    pass
+
+@entity
+class YCSB:
+    def __init__(self, key: str):
+        self.key: str = key
+        self.value: int = 1_000_000
+
+    def __key__(self):
+        return self.key
+
+    def get_key(self) -> str:
+        return self.key
+
+    def get_value(self) -> int: return self.value
+
+    def set_value(self, value: int):
+        self.value = value
+
+    def read(self) -> tuple[YCSB, int]:
+        return self, self.value
+
+    def update(self) -> tuple[YCSB, int]:
+        self.value += 1
+        return self, self.value
+
+    def update_t(self) -> None:
+        self.value += 1
+
+    def transfer(self, key_b: YCSB) -> tuple[YCSB, int]:
+
+        send_async(key_b.update_t())
+
+        self.value -= 1
+        if self.value < 0:
+            raise NotEnoughCredit(f"Not enough credit for user: {self}")
+
+        return self, self.value
